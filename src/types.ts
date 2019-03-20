8996714d0f4e090e0f5e89ed4f927d57fdbfbac0
Mikerah <mikerahqc@protonmail.com>
2019-03-20 08:56:06 -0400

    Syntax errors. Don't code while sleep deprived

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,17 @@
 // Defines the types used in the EWP
 
 export interface request {
-    version: string,
-    command: string,
-    response_compression: [string],
-    head_only_indicator: boolean,
-    headers: Buffer,
-    body: Buffer
+    version: string;
+    command: string;
+    response_compression: string[];
+    head_only_indicator: boolean;
+    headers: Buffer;
+    body: Buffer;
 }
 
 export interface response {
-    code: number,
-    compression: string,
-    headers: Buffer,
-    body: Buffer
+    code: number;
+    compression: string;
+    headers: Buffer;
+    body: Buffer;
 }
