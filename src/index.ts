e2ef95807d6aeddc6265c2ad8d3d947444eec70e
Mikerah <mikerahqc@protonmail.com>
2019-03-21 11:59:07 -0400

    Completed basic implementation of parsers

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ import {
 } from "./response_codes"
 
 function parseRequest(message: string) {
+    // TODO: Add appropriate error handling
     let requestLine, payload = message.split("\n");
     let requestFields = requestLine.split(" ");
 
@@ -27,24 +28,46 @@ function parseRequest(message: string) {
 	command: requestFields[2],
 	compression: requestFields[3],
 	response_compression: requestFields[4].split(","),
-	head_only_indicator: ,
-	headers:,
-	body:,
+	head_only_indicator: requestLine[7] === "H",
+	headers: Buffer.from(payload.splice(0, parseInt(requestLine[5]))),
+	body: Buffer.from(payload.splice(parseInt(requestLine[5]), parseInt(requestLine6) + parseInt(requestLine5))),
 
      };
      return request;
 }
 
+function createRequestStr(request: Request) {
+    return request.protocol + " " + 
+           request.version + " " +
+           request.command + " " +
+           request.compression + " " +
+           request.response_compression.toString() + " " +
+	   request.headers.length + " " + 
+           request.body.length + "\n" +
+           request.headers.concat([request.body])
+}
+
+
 function parseResponse(message: string) {
+    // TODO: Add appropriate error handling
     let responseLine, payload = message.split("\n");
     let responseFields = responseLine.split(" ");
 
     let response: Response = {
         code: parseInt(responseFields[0]),
 	compression: responseFields[1],
-	headers:,
-	body:,
-    }
+	headers: payload.slice(0, parseInt(responseLine[2])),
+	body: payload.slice(parseInt(responseLine[2]), parseInt(responseLine[3]) + parseInt(responseLine[2])),
+    };
+
+    return response;
 }
 
+function createResponseStr(response: Response) {
+    return response.code.toString() + " " +
+           response.compression + " " +
+           response.headers.length + " " +
+	   response.body.length + "\n" + 
+           response.headers.concat([response.body])
+}
 
